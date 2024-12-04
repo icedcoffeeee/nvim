e988000f75d502421149ff51e38f83fce02f22c2
icedcoffeeee <hazim.saharuddin@gmail.com>
2024-12-04 19:02:18 +0800

    hologram if linux

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,12 @@
+local hologram = {}
+if vim.loop.os_uname().sysname == "Linux" then
+	hologram = { "edluffy/hologram.nvim", opts = {} }
+end
+
 return {
 	{ "windwp/nvim-ts-autotag", opts = {} },
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 	"mg979/vim-visual-multi",
 	"tpope/vim-surround",
-	{ "edluffy/hologram.nvim", opts = {} },
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	hologram,
 }
