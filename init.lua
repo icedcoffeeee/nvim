b77668d49eab732ebbcd87a506e07d28d6dcecb7
icedcoffeeee <hazim.saharuddin@gmail.com>
2025-01-03 19:07:17 +0800

    actual multicursor

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@ require("config.options")
 require("config.mappings")
 require("config.lazy")
 
-vim.cmd[[colorscheme onedark]]
-vim.cmd[[:TransparentDisable]]
+vim.cmd([[colorscheme onedark]])
+vim.cmd([[:TransparentDisable]])
