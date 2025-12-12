194111867c00c8ef3f5be01e7f0c112810d77cb2
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-12-12 23:41:09 +0800

    styling

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,34 +3,18 @@ return {
 		"navarasu/onedark.nvim",
 		lazy = false,
 		priority = 100,
-		opts = {
-			style = "deep",
-		},
+		opts = {},
 	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 100,
-		opts = {
-			-- transparent = true,
-			styles = {
-				sidebars = "transparent",
-				float = "transparent",
-			},
-		},
-	},
-	{
-		"Mofiqul/vscode.nvim",
-		lazy = false,
-		priority = 100,
-	},
-	"xiyaowong/transparent.nvim",
 	{
 		"akinsho/bufferline.nvim",
 		opts = {
 			options = {
-				indicator = { style = "underline" },
 				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+				separator_style = "slant",
 				sort_by = "insert_after_current",
 			},
 		},
