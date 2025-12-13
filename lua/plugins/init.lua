41733209c37a64780d0433818ac7b563755a9dd9
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-12-13 17:15:04 +0800

    lazy to pckr :P

--- lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-local hologram = {}
-if vim.loop.os_uname().sysname == "Linux" then hologram = { "edluffy/hologram.nvim", opts = {} } end
-
-return {
-	{ "windwp/nvim-ts-autotag", opts = {} },
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-	"tpope/vim-surround",
-	hologram,
-}
